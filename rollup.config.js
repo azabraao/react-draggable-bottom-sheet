fbde034c26c4c64c228239818aabeda163304f13
Abraão Azevedo <az.abraao@gmail.com>
2022-07-27 14:37:12 -0300

    add and use rollup

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import css from "rollup-plugin-import-css";
+
+import typescript from "rollup-plugin-typescript2";
+
+import pkg from "./package.json";
+
+export default {
+  input: "src/index.tsx",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      exports: "named",
+      sourcemap: true,
+      strict: false,
+    },
+  ],
+  plugins: [css(), typescript({ objectHashIgnoreUnknownHack: true })],
+  external: ["react", "react-dom"],
+};
